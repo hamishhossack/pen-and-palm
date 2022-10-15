ff0ce9da72bc31879d967e6cd33f03bc189b5fea
Hamish Hossack <hamishhossack@gmail.com>
2022-10-15 13:10:59 +0100

    home title

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,11 @@ import SocialMedia from '../components/common/SocialMedia';
 import ExploreBanner from '../components/productAssets/ExploreBanner';
 import HeroSection from '../components/homepage/HeroSection';
 import HomeBanner from '../components/homepage/HomeBanner';
-import CategoryBanner from '../components/homepage/CategoryBanner';
-import ProductsBanner from '../components/homepage/ProductsBanner';
 
 const Home = () => (
   <Root transparentHeader={true}>
     <Head>
-      <title>Home | commerce</title>
+      <title>Home | Pen & Palm</title>
     </Head>
 
     <HeroSection />
