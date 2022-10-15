2b83cdd1e3ddeca7eac093bb5d5e1fdbd27d95fb
Hamish Hossack <hamishhossack@gmail.com>
2022-10-15 13:09:19 +0100

    prettier setup

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
-import React from "react";
-import Head from "next/head";
-import Root from "../components/common/Root";
-import Footer from "../components/common/Footer";
+import React from 'react';
+import Head from 'next/head';
+import Root from '../components/common/Root';
+import Footer from '../components/common/Footer';
 
 const About = () => (
   <Root>
