d73c163416e119199d54a69f6cbf77a5742c3835
Hamish Hossack <hamishhossack@gmail.com>
2024-02-07 09:42:25 +0000

    update doc titles

--- pages/_document.js
+++ pages/_document.js
@@ -36,30 +36,28 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="stylesheet" href="https://unpkg.com/swiper@6.6.2/swiper-bundle.min.css" />
-          <meta name="title" content="Headless Commerce example with Netlify" key="title" />
+          <meta
+            name="title"
+            content="Pen and palm - Calligraphy, signage & stationery beautifully bespoke and uniquely you"
+            key="title"
+          />
           <meta
             name="description"
-            content="An open source headless commerce example powered by Commerce.js and Netlify. Start your headless commerce application now!"
+            content="Pen and palm - Calligraphy, signage & stationery beautifully bespoke and uniquely you"
           />
           <meta property="og:title" content="Commerce.js | Demo storefront" />
-          <meta
-            property="og:image"
-            content="https://cdn.chec.io/email/assets/marketing/demo-preview.png"
-          />
+          <meta property="og:image" content="https://penandplam.com/images/logo.svg" />
           <meta
             property="og:description"
-            content="A custom, open source demo storefront built with Next.js and Commerce.js. Take a look!"
+            content="Pen and palm - Calligraphy, signage & stationery beautifully bespoke and uniquely you"
           />
           <meta property="og:url" content="https://commercejs-demo-store.netlify.app" />
           <meta property="twitter:title" content="Commerce.js | Demo storefront" />
           <meta name="twitter:creator" content="@commercejs" />
-          <meta
-            property="twitter:image"
-            content="https://cdn.chec.io/email/assets/marketing/demo-preview.png"
-          />
+          <meta property="twitter:image" content="https://penandplam.com/images/logo.svg" />
           <meta
             property="twitter:description"
-            content="A custom, open source demo storefront built with Next.js and Commerce.js. Take a look!"
+            content="Pen and palm - Calligraphy, signage & stationery beautifully bespoke and uniquely you"
           />
           <meta name="twitter:card" content="summary_large_image" />
         </Head>
