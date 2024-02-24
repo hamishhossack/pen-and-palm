e4fe3401716248312eadbef8280750875e3bd462
Hamish Hossack <hamishhossack@gmail.com>
2024-02-24 09:19:07 +0000

    use form action submit

--- components/productAssets/ContactBanner.js
+++ components/productAssets/ContactBanner.js
@@ -1,8 +1,6 @@
-import { useRouter } from 'next/router';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 export const ContactBanner = ({ formName }) => {
-  const [submitted, setSubmitted] = useState();
   const contactContainer = React.createRef();
   const image = React.createRef();
   const animate = useCallback(() => {
@@ -25,20 +23,6 @@ export const ContactBanner = ({ formName }) => {
     window.requestAnimationFrame(animate);
   }, [animate]);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => setSubmitted(true))
-      .catch((error) => alert(error));
-  };
-
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
@@ -51,58 +35,51 @@ export const ContactBanner = ({ formName }) => {
         <div ref={contactContainer} className="custom-container py-md-5">
           <div className="row py-5">
             <div className="col-12 col-md-6">
-              {submitted ? (
-                <>
-                  <h3 className="mb-4 font-family-secondary">Thank you!</h3>
-                  <p className="mb-4">I aim to reply with 48 hours</p>
-                </>
-              ) : (
-                <form name={formName} method="POST" data-netlify="true" onSubmit={handleSubmit}>
-                  <input type="hidden" name="form-name" value={formName} />
-                  <h3 className="mb-4 font-family-secondary">I&apos;d love to hear from you</h3>
-                  <p className="mb-2">
-                    You can get in touch via the form below or directly by emailing&nbsp;
-                    <a href="mailto: info@penandpalm.com">hello@penandpalm.com</a>.
-                  </p>
-                  <p className="mb-4">
-                    Please include as much detail as possible about your special day.
-                  </p>
-                  <div className="row align-items-center mb-2">
-                    <label className="col-md-3 py-3">Name</label>
-                    <input className="col-md-9 form-control" type="text" name="name" />
-                  </div>
-                  <div className="row align-items-center mb-2">
-                    <label className="col-md-3 py-3">Email</label>
-                    <input className="col-md-9 form-control" type="text" name="email" />
-                  </div>
-                  <div className="row align-items-center mb-2">
-                    <label className="col-md-3 py-3">Event date</label>
-                    <input className="col-md-9 form-control" type="text" name="event" />
-                  </div>
-                  <div className="row align-items-center mb-2">
-                    <label className="col-md-3 py-3">Guest count</label>
-                    <input className="col-md-9 form-control" type="number" name="guests" />
-                  </div>
-                  <div className="row align-items-center mb-2">
-                    <label className="col-md-3 py-3">Message</label>
-                    <textarea
-                      className="col-md-9 form-control"
-                      rows="6"
-                      name="message"
-                      placeholder="please include any requirements, ideas or preferences you have."
-                    />
-                  </div>
-                  <div className="row">
-                    <button
-                      type="submit"
-                      className="col-4 ml-auto py-3 align-items-center font-color-black borderbottom border-color-black"
-                    >
-                      <span className="mr-3">Send</span>
-                      <img src="/icon/arrow-long-right.svg" />
-                    </button>
-                  </div>
-                </form>
-              )}
+              <form name={formName} method="POST" action="/success" data-netlify="true">
+                <input type="hidden" name="form-name" value={formName} />
+                <h3 className="mb-4 font-family-secondary">I&apos;d love to hear from you</h3>
+                <p className="mb-2">
+                  You can get in touch via the form below or directly by emailing&nbsp;
+                  <a href="mailto: info@penandpalm.com">hello@penandpalm.com</a>.
+                </p>
+                <p className="mb-4">
+                  Please include as much detail as possible about your special day.
+                </p>
+                <div className="row align-items-center mb-2">
+                  <label className="col-md-3 py-3">Name</label>
+                  <input className="col-md-9 form-control" type="text" name="name" />
+                </div>
+                <div className="row align-items-center mb-2">
+                  <label className="col-md-3 py-3">Email</label>
+                  <input className="col-md-9 form-control" type="text" name="email" />
+                </div>
+                <div className="row align-items-center mb-2">
+                  <label className="col-md-3 py-3">Event date</label>
+                  <input className="col-md-9 form-control" type="text" name="event" />
+                </div>
+                <div className="row align-items-center mb-2">
+                  <label className="col-md-3 py-3">Guest count</label>
+                  <input className="col-md-9 form-control" type="number" name="guests" />
+                </div>
+                <div className="row align-items-center mb-2">
+                  <label className="col-md-3 py-3">Message</label>
+                  <textarea
+                    className="col-md-9 form-control"
+                    rows="6"
+                    name="message"
+                    placeholder="please include any requirements, ideas or preferences you have."
+                  />
+                </div>
+                <div className="row">
+                  <button
+                    type="submit"
+                    className="col-4 ml-auto py-3 align-items-center font-color-black borderbottom border-color-black"
+                  >
+                    <span className="mr-3">Send</span>
+                    <img src="/icon/arrow-long-right.svg" />
+                  </button>
+                </div>
+              </form>
             </div>
             <div className="col-md-6">
               <div ref={image} className="home-contact-banner--image ml-md-4 mt-4" />
